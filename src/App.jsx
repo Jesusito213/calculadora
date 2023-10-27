2975f6a1a962d0a5375b6197f676715a76a37169
Santi <gussxsat@gmail.com>
2023-10-27 16:06:08 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import "./App.css";
+import Boton from "./components/Boton";
+import Pantalla from "./components/Pantalla";
+import BotonClear from "./components/BotonClear";
+import { useState } from "react";
+import { evaluate } from "mathjs";
+
+function App() {
+	const [input, setInput] = useState("");
+
+	const agregarInput = (val) => {
+		setInput(input + val);
+	};
+
+	const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("Por favor ingrese un valor")
+    }
+	};
+
+	return (
+		<div className="App">
+			<div className="freecodecamp-logo-contendor">
+				<img
+					className="freecodecamp-logo"
+					src="/imagenes/freecodecamp-logo.png"
+					alt="Logo de FreeCodeCamp"
+				/>
+			</div>
+			<div className="contenedor-calculadora">
+				<Pantalla input={input} />
+				<div className="fila">
+					<Boton manejarClick={agregarInput}>1</Boton>
+					<Boton manejarClick={agregarInput}>2</Boton>
+					<Boton manejarClick={agregarInput}>3</Boton>
+					<Boton manejarClick={agregarInput}>+</Boton>
+				</div>
+				<div className="fila">
+					<Boton manejarClick={agregarInput}>4</Boton>
+					<Boton manejarClick={agregarInput}>5</Boton>
+					<Boton manejarClick={agregarInput}>6</Boton>
+					<Boton manejarClick={agregarInput}>-</Boton>
+				</div>
+				<div className="fila">
+					<Boton manejarClick={agregarInput}>7</Boton>
+					<Boton manejarClick={agregarInput}>8</Boton>
+					<Boton manejarClick={agregarInput}>9</Boton>
+					<Boton manejarClick={agregarInput}>*</Boton>
+				</div>
+				<div className="fila">
+					<Boton manejarClick={calcularResultado}>=</Boton>
+					<Boton manejarClick={agregarInput}>0</Boton>
+					<Boton manejarClick={agregarInput}>.</Boton>
+					<Boton manejarClick={agregarInput}>/</Boton>
+				</div>
+				<div className="fila">
+					<BotonClear manejarClear={() => setInput("")}>Clear</BotonClear>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
